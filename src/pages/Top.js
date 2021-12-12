7151daf210b225984b83d6c174649ff55250393e
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-12 23:44:56 +0900

    create global state

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,14 +1,17 @@
-import React, { useEffect} from 'react'
+import React, { useContext, useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
-import { fetchPopularData } from '../apis'
+import { fetchPopularData } from '../apis/index'
+import { Store } from '../store/index'
 
 const Top = () => {
+    const { globalState, setGlobalState} = useContext(Store)
+
     useEffect(() => {
         fetchPopularData().then((res) => {
             console.log('data', res)
+            setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
         })
     }, [])
-
     return (
         <Layout>
             top page
