da223521899c299a21ce3621b3bfbeb4a43a2132
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-14 00:33:10 +0900

    create List

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect} from 'react'
 import Layout from '../components/Layout/Layout'
 import { fetchPopularData } from '../apis/index'
 import { Store } from '../store/index'
+import VideoGrid from '../components/VideoGrid/VideoGrid'
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem'
 
 const Top = () => {
     const { globalState, setGlobalState} = useContext(Store)
@@ -14,7 +16,19 @@ const Top = () => {
     }, [])
     return (
         <Layout>
-            top page
+            <VideoGrid>
+                {
+                    globalState.popular && globalState.popular.map((popular) => {
+                        return (
+                            <VideoGridItem
+                                id={popular.id}
+                                key={popular.id}
+                                src={popular.snippet.thumbnails.standard.url}
+                                title={popular.snippet.title}/>
+                        )
+                    })
+                }
+            </VideoGrid>
         </Layout>
     )
 }
